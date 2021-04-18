6c1165d55eae21b434b278c8c1a05e6ca343370d
Guillermo <guillearnautm@gmail.com>
2021-04-18 17:12:05 +0200

    marselo to data

--- app/src/main/java/com/guitmcode/covidstats/CovidDialog.kt
+++ app/src/main/java/com/guitmcode/covidstats/CovidDialog.kt
@@ -2,24 +2,23 @@ package com.guitmcode.covidstats
 
 import android.app.AlertDialog
 import android.app.Dialog
-import android.content.DialogInterface
 import android.os.Bundle
 import androidx.fragment.app.DialogFragment
 
 
-class CovidDialog (private val marselo : CovidData) : DialogFragment() {
+class CovidDialog (private val data : CovidData) : DialogFragment() {
 	override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
 		if (activity == null)
 			throw IllegalStateException("Activity cannot be null")
 		val builder = AlertDialog.Builder(activity)
 		val gigamarselo = arrayOf("                                           Today      Total",
-			"Confirmed positives:     ${marselo.confirmedCases}   ${marselo.tt_confirmedCases}",
-			"Confirmed deaths:         ${marselo.deaths}          ${marselo.tt_deaths}",
-			"Hospitalised:                 ${marselo.hospitalized}          ${marselo.tt_hospitalized}",
-			"Intensive Care Unit:      ${marselo.ICU}                ${marselo.tt_ICU}",
-			"Open cases:                  ${marselo.openCases}   ${marselo.tt_openCases}",
-			"Recovered:                    ${marselo.recovered}                   ${marselo.tt_recovered}")
-		builder.setTitle(marselo.date)
+			"Confirmed positives:     ${data.confirmedCases}   ${data.tt_confirmedCases}",
+			"Confirmed deaths:         ${data.deaths}          ${data.tt_deaths}",
+			"Hospitalised:                 ${data.hospitalized}          ${data.tt_hospitalized}",
+			"Intensive Care Unit:      ${data.ICU}                ${data.tt_ICU}",
+			"Open cases:                  ${data.openCases}   ${data.tt_openCases}",
+			"Recovered:                    ${data.recovered}                   ${data.tt_recovered}")
+		builder.setTitle(data.date)
 			.setItems(gigamarselo) { dialog, which ->
 				
 			}
